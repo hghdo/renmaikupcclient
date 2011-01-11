5a5092da2ebb8eb9c608b2e0289fab44fb31b94b
Huang Gehua <bolixie@gmail.com>
2011-01-11 22:23:44 +0800

    update

---
+++ pcclient/ImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media.Imaging;
+using System;
+
+public sealed class ImageConverter : System.Windows.Data.IValueConverter
+{
+    public object Convert(object value, Type targetType,
+                          object parameter, System.Globalization.CultureInfo culture)
+    {
+        try
+        {
+            return new BitmapImage(new Uri((string)value));
+        }
+        catch
+        {
+            return new BitmapImage();
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType,
+                              object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
