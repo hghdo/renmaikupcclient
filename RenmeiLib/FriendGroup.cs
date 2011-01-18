d22ea349a585dafb5a41faa412c5d4a2a4909efa
Huang Gehua <bolixie@gmail.com>
2011-01-18 23:02:31 +0800

    add FriendGroup class for group

---
+++ RenmeiLib/FriendGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RenmeiLib
+{
+    /// <summary>
+    /// Represents the status post for a Twitter User.
+    /// </summary>
+    [Serializable]
+    public class FriendGroup : INotifyPropertyChanged, IEquatable<FriendGroup>
+    {
+
+
+    }
+
+    /// <summary>
+    /// Collection of Tweets
+    /// </summary>
+    [Serializable]
+    public class TweetCollection : ObservableCollection<FriendGroup>
+    {
+
+    }
+
+}
