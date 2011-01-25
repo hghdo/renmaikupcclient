cd677e3d8ae26ccfb6fd74c0298df4c1c328d081
Huang Gehua <bolixie@gmail.com>
2011-01-25 23:01:31 +0800

    add send comment function

---
+++ RenmeiLib/Comment.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace RenmeiLib
+{
+    [Serializable]
+    public class Comment : INotifyPropertyChanged
+    {
+        #region Private fields
+        private long id;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        private DateTime publishedTime;
+
+        public DateTime PublishedTime
+        {
+            get { return publishedTime; }
+            set { publishedTime = value; }
+        }
+
+        private string clientType;
+
+        public string ClientType
+        {
+            get { return clientType; }
+            set { clientType = value; }
+        }
+
+        private User user;
+
+        public User User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+        #endregion
+
+
+        #region IEquatable<Comment> Members
+
+        /// <summary>
+        /// Tweets are the same if they have the same Id.
+        /// Collection.Contains needs IEquatable implemented to be effective. 
+        /// </summary>
+        public bool Equals(Comment other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            if (id != other.id)
+                return false;
+            if (Id == -1)  // special type of tweet, so compare the user and text
+                return ((Id == other.Id) && (text == other.Text));
+
+            return true;
+        }
+
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            Comment g = obj as Comment;
+            if (g != null)
+            {
+                return Equals(g);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+
+
+    /// <summary>
+    /// Collection of FirendGroups
+    /// </summary>
+    [Serializable]
+    public class CommentCollection : ObservableCollection<Comment>
+    {
+
+    }
+
+}
---
+++ pcclient/CommentsWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CustomWindow;
+using RenmeiLib;
+
+namespace pcclient
+{
+    /// <summary>
+    /// Interaction logic for CommentsWindow.xaml
+    /// </summary>
+    public partial class CommentsWindow : EssentialWindow
+    {
+
+        public IServiceApi twitterApi;
+
+        public Tweet tweet;
+
+        public CommentCollection comments;
+
+        public CommentsWindow()
+        {
+            InitializeComponent();
+            //HeadImage.Source = new BitmapImage(new Uri(tweet.User.ImageUrl)); ;
+
+            TitleBar.DataContext = tweet;
+        }
+
+        public CommentsWindow(IServiceApi api, Tweet t)
+        {
+            twitterApi = api;
+            tweet = t;
+            InitializeComponent();
+            comments = new CommentCollection();
+            //HeadImage.Source = new BitmapImage(new Uri(tweet.User.ImageUrl)); ;
+            TitleBar.DataContext = tweet;
+            TweetText.DataContext = tweet;
+            CommentsListBox.DataContext = comments;
+            UpdateComments();
+        }
+
+        protected override Decorator GetWindowButtonsPlaceholder()
+        {
+            return WindowButtonsPlaceholder;
+        }
+
+        private void Header_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void Thumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            if (this.Width + e.HorizontalChange > 10)
+                this.Width += e.HorizontalChange;
+            if (this.Height + e.VerticalChange > 10)
+                this.Height += e.VerticalChange;
+        }
+
+        private void Comment_Click(object sender, RoutedEventArgs e)
+        {
+            twitterApi.AddComment(NewCommentBox.Text, tweet);
+            UpdateComments();
+        }
+
+        private void CancelComment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateComments()
+        {
+            CommentCollection newComments = new CommentCollection();
+            newComments = twitterApi.RetriveComments(tweet);
+            for (int i = newComments.Count - 1; i >= 0; i--)
+            {
+                Comment co = newComments[i];
+                if (comments.Contains(co)) continue;
+                comments.Add(co);
+            }
+
+        }
+
+    }
+}
