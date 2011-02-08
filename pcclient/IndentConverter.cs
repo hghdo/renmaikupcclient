589c494e0052d4251cd63ee1b483fbdeceb14ea0
bill4mobile <bill4mobile@gmail.com>
2011-02-08 22:54:37 +0800

    To be search friend implement

--- pcclient/IndentConverter.cs
+++ pcclient/IndentConverter.cs
@@ -6,22 +6,39 @@ using System.Windows.Data;
 using System.Globalization;
 using System.Windows.Controls;
 using System.Windows;
+using System.Windows.Media;
 
 namespace pcclient
 {
     public class IndentConverter : IValueConverter
     {
         public double Indent { get; set; }
+        //public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        //{
+        //    var item = value as TreeViewItem;
+        //    if (null == item)
+        //        return new Thickness(0);
+        //    double ind = Indent * item.GetDepth();
+        //    App.Logger.Debug(string.Format("indent {0}", ind));
+
+        //    return new Thickness(ind, 0, 0, 0);
+        //}
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var item = value as TreeViewItem;
-            if (null == item)
-                return new Thickness(0);
-            double ind = Indent * item.GetDepth();
-            App.Logger.Debug(string.Format("indent {0}", ind));
-
-            return new Thickness(ind, 0, 0, 0);
+            var level = -1;
+            if (value is DependencyObject)
+            {
+                var parent = VisualTreeHelper.GetParent(value as DependencyObject);
+                while (!(parent is TreeView) && (parent != null))
+                {
+                    if (parent is TreeViewItem)
+                        level++;
+                    parent = VisualTreeHelper.GetParent(parent);
+                }
+            }
+            return level;
         }
+
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
